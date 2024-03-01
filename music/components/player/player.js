8c7f0b012d52a4f8a55a9e6c3ecd0ba71855f6b5
Ankit Khanal <akhanal749@gmail.com>
2024-03-01 22:54:55 +0545

    added alt in images

--- music/components/player/player.js
+++ music/components/player/player.js
@@ -21,27 +21,45 @@ export default function Player() {
   return (
     <>
       <div className="player">
-        <Image src="/im.png" height={200} width={250} style={{objectFit:"cover"}} />
+        <Image
+          src="/im.png"
+          height={200}
+          width={250}
+          style={{ objectFit: "cover" }}
+          alt="icon"
+        />
         <div className="details">
-          <p className="name" style={{fontSize:"20px",fontWeight:"bold"}}>Name</p>
-          <p className="artist" style={{fontSize:"16px"}}>Artist</p>
+          <p className="name" style={{ fontSize: "20px", fontWeight: "bold" }}>
+            Name
+          </p>
+          <p className="artist" style={{ fontSize: "16px" }}>
+            Artist
+          </p>
         </div>
         <div className="controls">
           <div className="buttons">
             <button>
-              <Image src="/previous.png" height={20} width={20} />
+              <Image src="/previous.png" height={20} width={20} alt="icon" />
             </button>
-            <button className="play" onClick={changePlayPause} style={{height:"50px",width:"50px"}}>
-              <Image src={playPause} height={25} width={25} />
+            <button
+              className="play"
+              onClick={changePlayPause}
+              style={{ height: "50px", width: "50px" }}
+            >
+              <Image src={playPause} height={25} width={25} alt="icon"/>
             </button>
             <button>
-              <Image src="/next.png" height={20} width={20} />
+              <Image src="/next.png" height={20} width={20} alt="icon"/>
             </button>
           </div>
           <div className="bar">
-            <input type="range" className="range" style={{width:"300px"}}/>
-            <button className="volume" onClick={changeMute} style={{position:"absolute",marginLeft:"370px"}}>
-              <Image src={mute} height={20} width={20} />
+            <input type="range" className="range" style={{ width: "300px" }} />
+            <button
+              className="volume"
+              onClick={changeMute}
+              style={{ position: "absolute", marginLeft: "370px" }}
+            >
+              <Image src={mute} height={20} width={20} alt="icon"/>
             </button>
           </div>
           <p>
