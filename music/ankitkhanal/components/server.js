3e82cfde1de6b0b6ad9d055cc0ba9fe20182550b
Ankit Khanal <akhanal749@gmail.com>
2024-03-05 22:56:51 +0545

    added

--- music/ankitkhanal/components/server.js
+++ music/ankitkhanal/components/server.js
@@ -1,7 +1,7 @@
 async function fetchWebApi(endpoint, method, body) {
   const res = await fetch(`https://api.spotify.com/${endpoint}`, {
     headers: {
-      Authorization: `Bearer BQBQc_4tWy2Y35QJqXyQUGd7kIC0tvzKBYL43L_T8hbt_uWFXjoVj1dnQCypM0yxf6duRGAt7G1wuIjoBDjonibKE4GIHFbFyEsRSuSF_Mv5_fTN7oFkfWbdFueyCSP-Hl7qwprkCvZRezUsFP7YiVrGdgj6nRxU7kmf2R2G_hICZMIbCQ379_3rW5TMAjUxNKFDe8IUT4D2XxPNEq9cq3g5VMYUN5era8pYAFKUN6UgiaFKIlfY16PR4LTnFHZH04dKuOE1LAkP1dDcwpriUSic`,
+      Authorization: `Bearer BQDJanjBQ8wmgdsLBGf1xnpJWGXfQYH6jXP3XnnORC2ZgUYuS_T2QGqleVb994fJobK5Y6Zx664ysQ5JsgUjipHuiMoiOoXUqqbixWVY21jgC6g-6ZrCnAaYZv9ZnSQQKYTzk7oXaJKYosZSHK-W76GUHePl9zzkLDwBLm8_Pw_qicxOV_e1NQmj4p6XhnwxJSbkse6YAqSoPF_BhQJT9EhXdixGR-GkXWxgXT7hV868YNRgD1q_3j4nofasfkgvi-Cls6akclEWcfwRhhM4pcwD`,
     },
     method,
     body:JSON.stringify(body)
@@ -16,4 +16,5 @@ async function getTopTracks(){
 }
 
 const topTracks = await getTopTracks();
+console.log(topTracks)
 export default topTracks;
