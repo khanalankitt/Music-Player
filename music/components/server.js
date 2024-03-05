603bb38741be4112d57f9666b004dd04435990e1
Ankit Khanal <akhanal749@gmail.com>
2024-03-05 22:38:55 +0545

    added

--- music/components/server.js
+++ music/components/server.js
@@ -1,7 +1,7 @@
 async function fetchWebApi(endpoint, method, body) {
   const res = await fetch(`https://api.spotify.com/${endpoint}`, {
     headers: {
-      Authorization: `Bearer ${process.env.TOKEN}`,
+      Authorization: `Bearer BQBQc_4tWy2Y35QJqXyQUGd7kIC0tvzKBYL43L_T8hbt_uWFXjoVj1dnQCypM0yxf6duRGAt7G1wuIjoBDjonibKE4GIHFbFyEsRSuSF_Mv5_fTN7oFkfWbdFueyCSP-Hl7qwprkCvZRezUsFP7YiVrGdgj6nRxU7kmf2R2G_hICZMIbCQ379_3rW5TMAjUxNKFDe8IUT4D2XxPNEq9cq3g5VMYUN5era8pYAFKUN6UgiaFKIlfY16PR4LTnFHZH04dKuOE1LAkP1dDcwpriUSic`,
     },
     method,
     body:JSON.stringify(body)
