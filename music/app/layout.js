0629c5cb40d7fa48fcd0dff8460e8b7179df9c79
Ankit Khanal <akhanal749@gmail.com>
2024-03-05 22:13:37 +0545

    added

--- music/app/layout.js
+++ music/app/layout.js
@@ -2,7 +2,7 @@ import { Inter } from "next/font/google";
 import "../styles/style.css"
 const inter = Inter({ subsets: ["latin"] });
 export const metadata = {
-  title: "Music Player",
+  title: "Spotify Music Player",
   description: "A simple, minimal and lightweight music player cerated by using Next JS",
 };
 
